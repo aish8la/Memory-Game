827548fdcfc5c30da50191e2c5f6ffb161268c15
aish8la <64914670+aish8la@users.noreply.github.com>
2025-08-13 02:27:27 +0500

    feat: extract data fetching to utility function

---
+++ src/utils/pokemonData.js
@@ -0,0 +1,72 @@
+const apiString = "https://pokeapi.co/api/v2/pokemon";
+const limit = 12;
+
+function apiStringFill(limit, offset) {
+    return apiString+"?limit="+limit+"&offset="+offset;
+}
+
+class CardClass {
+    constructor(id, name, imgURL) {
+        this.id = id;
+        this.name = name;
+        this.imgURL = imgURL;
+        this.selected = false;
+    }
+
+    selectToggle() {
+        if(!this.selected) this.selected = true;
+    }
+}
+
+function randomNum(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+async function fetchPokemonData(signal) {
+
+    const api = apiStringFill(limit, randomNum(200));
+    let pokemonLinks;
+    try {
+        const listAPIResponse = await fetch(api, {signal});
+        const listJSON = await listAPIResponse.json();
+        pokemonLinks = listJSON.results;
+
+    } catch (error) {
+        console.error("Error occurred in fetching: ", error);
+    }
+
+    const promises = pokemonLinks.map( link => {
+        return fetch(link.url, {signal})
+            .then(response => response.json())
+            .catch(err => {
+                console.error("Failed to fetch Data: ", err);
+            });
+    });
+
+    const resolvedList = await Promise.all(promises);
+    return resolvedList;
+}
+
+export async function loadPokemonData(controllerSignal) {
+    try {
+        const resolvedList = await fetchPokemonData(controllerSignal);
+        const pokemonCardArray = resolvedList.map( entry => {
+            const requiredData = {
+                id: entry.id,
+                name: entry.name,
+                imgURL: entry.sprites.other["official-artwork"].front_default
+            }
+
+            return new CardClass(
+                requiredData.id,
+                requiredData.name,
+                requiredData.imgURL
+            )
+        });
+
+        return pokemonCardArray;
+    } catch(err) {
+        console.error("Failed to load data: ", err);
+    }
+
+}
