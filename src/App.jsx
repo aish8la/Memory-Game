97f6f8fa2805a1ad0a8469eb5129a873209fb7b6
aish8la <64914670+aish8la@users.noreply.github.com>
2025-08-13 22:24:27 +0500

    feat: add state for current display

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,23 @@ import { useState } from "react";
 export default function App() {
   
   const [score, setScore] = useState({currentScore: 0, highScore: 0});
+  const [display, setDisplay] = useState("MAIN");
+
+  let currentDisplay;
+  
+  switch(display) {
+    case "MAIN":
+    case "GAME":
+      currentDisplay = <Game scoreSetter={setScore} ></Game>;
+      break;
+    case "OVER":
+    default:
+      break;
+  }
 
   return (
     <Layout score={score}>
-      <Game scoreSetter={setScore} ></Game>
+      {currentDisplay}
     </Layout>
   )
 }
